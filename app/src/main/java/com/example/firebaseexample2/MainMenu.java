3bdc3bc4af01b98bf9a1b1c082377903a4e22f00
DESKTOP-RPN6QGT <vryan8822@gmail.com>
2022-12-20 23:13:11 +0200

    uploaded png profile pic

--- app/src/main/java/com/example/firebaseexample2/MainMenu.java
+++ app/src/main/java/com/example/firebaseexample2/MainMenu.java
@@ -1,18 +1,42 @@
 package com.example.firebaseexample2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 public class MainMenu extends AppCompatActivity {
 
     Button signOut;
+    ImageView profileImage;
+    Button uploadBtn;
+    ProgressBar progressBar;
+
+
+    Uri imageUri;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser user = auth.getCurrentUser();
+    StorageReference storageReference;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -20,7 +44,28 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        storageReference = FirebaseStorage.getInstance().getReference(Objects.requireNonNull(user.getEmail()));
+
         signOut = findViewById(R.id.button);
+        profileImage = findViewById(R.id.profile_imageView);
+        uploadBtn = findViewById(R.id.upload_profile_pic_btn);
+        progressBar = findViewById(R.id.progressBar);
+
+        progressBar.setVisibility(View.INVISIBLE);
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fileChooser();
+            }
+        });
+
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +76,51 @@ public class MainMenu extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void fileChooser(){
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(i, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode ==1 && resultCode == RESULT_OK){
+            assert data != null;
+            imageUri = data.getData();
+            Picasso.get().load(imageUri).into(profileImage);
+
+        }
+    }
+
+    public void uploadPhoto(){
+        uploadBtn.setClickable(false);
+        progressBar.setVisibility(View.VISIBLE);
+
+        StorageReference reference = storageReference.child("user");
+        reference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+
+                    }
+                });
+
     }
 }
